ae69e48a64bc8391464e3526a25d915ef87749c2
GM-Coder007 <anomynus575@gmail.com>
2023-03-10 16:08:27 +0100

    Dodan header, knjižnice, moduli

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,16 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {useState} from "react";
 import {UserContext} from "./userContext";
 import Home from "./components/Home";
+<<<<<<< Updated upstream
 
 
+=======
+import HomeCplusPlus from "./components/cPlusPlus/cPlusPlusHomepage";
+import HomeCplusPlusEasy from "./components/cPlusPlus/cPlusPlusEasy";
+import HomeCplusPlusMedium from "./components/cPlusPlus/cPlusPlusMedium";
+import HomeCplusPlusHard from "./components/cPlusPlus/cPlusPlusHard";
+import HtmlHomepage from "./components/html/htmlHomepage";
+>>>>>>> Stashed changes
 function App() {
 
     const [user, setUser] = useState(localStorage.user ? JSON.parse(localStorage.user) : null);
@@ -19,6 +27,14 @@ function App() {
                     
                     <Routes>  
                         <Route path={"/"} exact element={<Home/>}></Route>
+<<<<<<< Updated upstream
+=======
+                        <Route path={"/cPlusPlus"} exact element={<HomeCplusPlus/>}></Route>
+                        <Route path={"/cPlusPlus/Easy"} exact element={<HomeCplusPlusEasy/>}></Route>
+                        <Route path={"/cPlusPlus/Medium"} exact element={<HomeCplusPlusMedium/>}></Route>
+                        <Route path={"/cPlusPlus/Hard"} exact element={<HomeCplusPlusHard/>}></Route>
+                        <Route path={"/html"} exact element={<HtmlHomepage/>}></Route>
+>>>>>>> Stashed changes
 
                     </Routes>
                 </UserContext.Provider>
---
+++ frontend/src/components/html/htmlHomepage.js
@@ -0,0 +1,12 @@
+import Header from "./htmlHeader";
+
+function HtmlHomepage() {
+    return (
+        <div>
+        
+        <Header title="HTML Course"></Header>  
+        </div>
+    );
+
+}
+export default HtmlHomepage;
