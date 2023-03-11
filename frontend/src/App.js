9bb3a6ef26b12233cb0c88548a4783ae46e51a20
Pungz <timotej.pun123@gmail.com>
2023-03-11 12:03:25 +0100

    Setting up for merge pt. 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
+import './index.css';
+import Header from './components/Header/Header';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {useState} from "react";
+import React, {useState} from "react";
 import {UserContext} from "./userContext";
 import Home from "./components/Home";
-
-
 import HomeCplusPlus from "./components/cPlusPlus/cPlusPlusHomepage";
 import HomeCplusPlusEasy from "./components/cPlusPlus/cPlusPlusEasy";
 import HomeCplusPlusMedium from "./components/cPlusPlus/cPlusPlusMedium";
 import HomeCplusPlusHard from "./components/cPlusPlus/cPlusPlusHard";
-import HtmlHomepage from "./components/html/htmlHomepage";
-import CssHomepage from "./components/css/cssHomepage";
-import HtmlEasyHomepage from "./components/html/htmlEasy/htmlEasyHomepage";
-import HtmlEasyCourse from "./components/html/htmlEasy/htmlEasyCourse";
-import HtmlEasyQuestions from "./components/html/htmlEasy/htmlEasyQuestions";
+import HomeSql from "./components/sql/HomeSql";
+import HomeSqlEasy from "./components/sql/HomeSqlEasy";
+import HomeSqlMedium from "./components/sql/HomeSqlMedium";
+import HomeSqlHard from "./components/sql/HomeSqlHard";
+import Navbar from "./components/sql/Navbar";
+import HomeJS from './components/javascript/js-home';
+import JSEasy from './components/javascript/js-easy';
+import JSMedium from './components/javascript/js-medium';
+import JSHard from './components/javascript/js-hard';
+import { routes } from './routes';
+
 function App() {
 
     const [user, setUser] = useState(localStorage.user ? JSON.parse(localStorage.user) : null);
@@ -25,19 +31,21 @@ function App() {
         return (
             <BrowserRouter>
                 <UserContext.Provider value={{user: user, setUserContext: updateUserData}}>
-                    
+
                     <Routes>  
                         <Route path={"/"} exact element={<Home/>}></Route>
                         <Route path={"/cPlusPlus"} exact element={<HomeCplusPlus/>}></Route>
                         <Route path={"/cPlusPlus/Easy"} exact element={<HomeCplusPlusEasy/>}></Route>
                         <Route path={"/cPlusPlus/Medium"} exact element={<HomeCplusPlusMedium/>}></Route>
                         <Route path={"/cPlusPlus/Hard"} exact element={<HomeCplusPlusHard/>}></Route>
-                        <Route path={"/html"} exact element={<HtmlHomepage/>}></Route>
-                        <Route path={"/css"} exact element={<CssHomepage/>}></Route>
-                        <Route path={"/html/EasyHomepage"} exact element={<HtmlEasyHomepage/>}></Route>
-                        <Route path={"/html/EasyCourse"} exact element={<HtmlEasyCourse/>}></Route>
-                        <Route path={"/html/EasyQuestions"} exact element={<HtmlEasyQuestions/>}></Route>
-
+                        <Route path={"/sql"} exact element={<HomeSql/>}></Route>
+                        <Route path={"/sql/Easy"} exact element={<HomeSqlEasy/>}></Route>
+                        <Route path={"/sql/Medium"} exact element={<HomeSqlMedium/>}></Route>
+                        <Route path={"/sql/Hard"} exact element={<HomeSqlHard/>}></Route>
+                        <Route path="/js" exact element={<HomeJS />}></Route>
+                        <Route path="/js/easy" exact element={<JSEasy />}></Route>
+                        <Route path="/js/medium" exact element={<JSMedium />}></Route>
+                        <Route path="/js/hard" exact element={<JSHard />}></Route>
                     </Routes>
                 </UserContext.Provider>
             </BrowserRouter>
